56458a032b13a271191aef010c8b47edf36249ab
GojoSanji21 <hiluhu7@gmail.com>
2025-04-18 10:05:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -66,7 +66,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True
 
 #Short Url or Api
 SHORT_URL = os.environ.get("SHORTNER_URL", "arolinks.com")
-SHORT_API = os.environ.get("SHORTNER_API", "87233ee8879a0c20fffb854e9c14052152133ed2")
+SHORT_API = os.environ.get("SHORTNER_API", "6181ee40bf341a08f8ca40f01b34c516b33ac548")
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - [ @Adult_Flux ]"
